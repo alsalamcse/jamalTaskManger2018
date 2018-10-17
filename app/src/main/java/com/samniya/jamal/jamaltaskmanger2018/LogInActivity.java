26f4c8f9c1de68a749e48b381f20e1db0453d44b
user <muhammed.awad@outlook.com>
2018-10-17 08:56:31 +0300

    2132

--- app/src/main/java/com/samniya/jamal/jamaltaskmanger2018/LogInActivity.java
+++ app/src/main/java/com/samniya/jamal/jamaltaskmanger2018/LogInActivity.java
@@ -40,4 +40,5 @@ public class LogInActivity extends AppCompatActivity {
     }
 
 
+
 }
--- app/src/main/java/com/samniya/jamal/jamaltaskmanger2018/SignUpActivity.java
+++ app/src/main/java/com/samniya/jamal/jamaltaskmanger2018/SignUpActivity.java
@@ -1,21 +1,33 @@
 package com.samniya.jamal.jamaltaskmanger2018;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private EditText firstNameET, lastNameET, phoneET, passwordET, emailET;
     private Button saveBtn;
 
+    private FirebaseAuth auth;
+    private FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        auth=FirebaseAuth.getInstance();
+        user=auth.getCurrentUser();
         firstNameET=findViewById(R.id.firstNameET);
         lastNameET=findViewById(R.id.lastNameET);
         phoneET=findViewById(R.id.phoneET);
@@ -32,4 +44,24 @@ public class SignUpActivity extends AppCompatActivity {
 
 
     }
+    private void creatAcount(String email, String passw) {
+        auth.createUserWithEmailAndPassword(email,passw).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(SignUpActivity.this, "Authentication Successful.", Toast.LENGTH_SHORT).show();
+                    //updateUserProfile(task.getResult().getUser());
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(SignUpActivity.this, "Authentication failed."+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+            }
+        });
+    }
+
+
 }
