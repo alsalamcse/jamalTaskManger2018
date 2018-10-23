e632a3d15e96c926c5a7be63130a78deeec925e6
user <muhammed.awad@outlook.com>
2018-10-23 13:21:16 +0300

    2132

---
+++ app/src/main/java/com/samniya/jamal/jamaltaskmanger2018/AddTaskActivity.java
@@ -0,0 +1,13 @@
+package com.samniya.jamal.jamaltaskmanger2018;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddTaskActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+    }
+}
