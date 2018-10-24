97a6fef53826b77c5db47ab47cf8b6058bb8f277
user <muhammed.awad@outlook.com>
2018-10-24 08:59:22 +0300

    2132

---
+++ app/src/main/java/MyTask.java
@@ -0,0 +1,80 @@
+import java.util.Date;
+
+public class MyTask {
+
+
+    private String key;
+    private String title;
+    private String text;
+    private int important;
+    private int necessary;
+    private Date createdAt;
+    private Date dueDate;
+
+    public MyTask(String key, String title, String text, int important, int necessary, Date createdAt, Date dueDate) {
+        this.key = key;
+        this.title = title;
+        this.text = text;
+        this.important = important;
+        this.necessary = necessary;
+        this.createdAt = createdAt;
+        this.dueDate = dueDate;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getImportant() {
+        return important;
+    }
+
+    public int getNecessary() {
+        return necessary;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setImportant(int important) {
+        this.important = important;
+    }
+
+    public void setNecessary(int necessary) {
+        this.necessary = necessary;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+}
+
--- app/src/main/java/com/samniya/jamal/jamaltaskmanger2018/AddTaskActivity.java
+++ app/src/main/java/com/samniya/jamal/jamaltaskmanger2018/AddTaskActivity.java
@@ -2,12 +2,55 @@ package com.samniya.jamal.jamaltaskmanger2018;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class AddTaskActivity extends Activity {
+
+    private EditText titleET,taskET,editText3;
+    private SeekBar seekBar1,seekBar2;
+    private TextView iTv,nTv;
+    private Button saveBtn1,datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        titleET=findViewById(R.id.titleET);
+        editText3=findViewById(R.id.editText3);
+        taskET=findViewById(R.id.taskET);
+        seekBar1=findViewById(R.id.seekBar1);
+        seekBar2=findViewById(R.id.seekBar2);
+        iTv=findViewById(R.id.iTv);
+        nTv=findViewById(R.id.nTv);
+        saveBtn1=findViewById(R.id.saveBtn1);
+        datePicker=findViewById(R.id.datePicker);
+
+        saveBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        datePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+
+
+
+
+
+
+
+
+
     }
 }
