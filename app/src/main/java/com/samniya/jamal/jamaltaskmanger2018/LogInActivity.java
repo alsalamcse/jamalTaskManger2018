0eb144f91b5381a40740b94285dd9f6398f97b7f
user <muhammed.awad@outlook.com>
2018-11-06 13:20:58 +0200

    2132

--- app/src/main/java/com/samniya/jamal/jamaltaskmanger2018/LogInActivity.java
+++ app/src/main/java/com/samniya/jamal/jamaltaskmanger2018/LogInActivity.java
@@ -1,11 +1,15 @@
 package com.samniya.jamal.jamaltaskmanger2018;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogInActivity extends AppCompatActivity{
 
@@ -13,6 +17,7 @@ public class LogInActivity extends AppCompatActivity{
     private EditText password;
     private Button signInBtn;
     private Button signUpBtn;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +45,26 @@ public class LogInActivity extends AppCompatActivity{
             }
         });
     }
+    private void signIn(String email, String passw) {
+        auth.signInWithEmailAndPassword(email,passw).addOnCompleteListener(LogInActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(LogInActivity.this, "signIn Successful.", Toast.LENGTH_SHORT).show();
+                    // Intent intent=new Intent(LogInActivity.this,MainTabsActivity.class);
+                    //   startActivity(intent);
+                    //  finish();
+                }
+                else
+                {
+                    Toast.makeText(LogInActivity.this, "signIn failed."+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+            }
+        });
+    }
+
 }
 
 
